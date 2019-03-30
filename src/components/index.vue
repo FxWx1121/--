a4bb01b5834a7a21bf6faff40be06f820d22657e
FxWx1121 <572931374@qq.com>
2019-03-30 16:42:57 +0800

    后台系统布局完成

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,13 +1,71 @@
 <template>
-    <div>首页</div>
+  <!-- 整体容器 -->
+  <el-container class="index-container">
+    <!-- 头部容器 -->
+    <el-header class="my-header">
+      <el-row>
+        <el-col :span="4">
+          <img src="../assets/logo.png" alt>
+        </el-col>
+        <el-col :span="18">
+          <h2>后台管理系统</h2>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="success">退出</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container class="my-container">
+      <!-- 左边内容 -->
+      <el-aside width="200px" class="my-aside">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-2"> <span class="el-icon-menu"></span> 选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主题区域 -->
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang='scss'>
+.index-container {
+  display: flex;
+  height: 100%;
+  .my-header {
+    height: 60px;
+    background-color: #b3c0d1;
+    line-height: 60px;
+    h2 {
+      margin: 0;
+      padding: 0;
+      text-align: center;
+      color: #fff;
+    }
+  }
+}
+.my-container {
+  flex: 1;
+  .my-aside {
+    background-color: skyblue;
+  }
+  .my-main {
+    background-color: pink;
+  }
+}
 </style>
