cd87025da35b17777a497c4de55515d85d80c185
FxWx1121 <572931374@qq.com>
2019-03-30 11:44:23 +0800

    整合ul框架css预处理

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>登录</div>
-    
+    <div class="login">登录</div>
+
 </template>
 
 <script>
@@ -9,6 +9,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+.login{
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入饿了么ul
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 //导入路由
