90abddfcd023522ebbe4412986de6e701f055719
FxWx1121 <572931374@qq.com>
2019-03-31 21:48:04 +0800

    完成用户列表的编辑,修改和删除等功能

--- src/components/users.vue
+++ src/components/users.vue
@@ -8,12 +8,17 @@
     </el-breadcrumb>
     <el-row>
       <el-col :span="6">
-        <el-input placeholder="请输入内容" v-model="sendData.query" class="input-with-select">
+        <el-input
+          placeholder="请输入内容"
+          v-model="sendData.query"
+          @keyup.native.enter="search"
+          class="input-with-select"
+        >
           <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
         </el-input>
       </el-col>
       <el-col :span="12">
-        <el-button type="success" plain>搜索</el-button>
+        <el-button type="success" @click="addFormVisible=true" plain>搜索</el-button>
       </el-col>
     </el-row>
     <!-- 表格 -->
@@ -25,7 +30,12 @@
       <el-table-column prop="mg_state" label="用户状态">
         <!-- scope 是一个名字 -->
         <template slot-scope="scope">
-          <el-switch @change="stateChange(scope.row)" v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          <el-switch
+            @change="stateChange(scope.row)"
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          ></el-switch>
         </template>
       </el-table-column>
       <el-table-column label="操作">
@@ -39,7 +49,13 @@
             @click="handleEdit(scope.$index, scope.row)"
             plain
           ></el-button>
-          <el-button type="danger" size="mini" icon="el-icon-delete" plain></el-button>
+          <el-button
+            @click="delOne(scope.row)"
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            plain
+          ></el-button>
           <el-button type="warning" size="mini" icon="el-icon-check" plain></el-button>
         </template>
       </el-table-column>
@@ -51,6 +67,45 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+    <!-- 新增对话框 -->
+    <el-dialog title="添加用户" :visible.sync="addFormVisible">
+      <el-form :model="addForm" :rules="addRules" ref="addForm">
+        <el-form-item label="用户名" label-width="100px" prop="username">
+          <el-input v-model="addForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="100px" prop="password">
+          <el-input v-model="addForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="addForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="addForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitAdd('addForm')">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 编辑对象 -->
+    <el-dialog title="修改用户" :visible.sync="editFormFormVisible">
+      <el-form :model="editForm" :rules="addRules" ref="editForm">
+        <el-form-item label="用户名" label-width="100px" prop="username">
+          <el-input v-model="editForm.username" disabled autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="editForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="editForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editFormFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitAdd('editForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -68,14 +123,48 @@ export default {
         pagesize: 10
       },
       //用户数组
-      userList: []
+      userList: [],
+      //是否显示新增对话框
+      addFormVisible: false,
+      //是否显示编辑对话框
+      editFormFormVisible: false,
+      //新增表单提交
+      addForm: {
+        username: "张三",
+        password: "123456",
+        email: "12306@qq.com",
+        mobile: "18339155966"
+      },
+      //编辑单提交
+      editForm: {
+        username: "三给",
+        email: "12306@qq.com",
+        mobile: "18339155966"
+      },
+      //新增表单验证规则
+      addRules: {
+        username: [
+          { required: true, message: "用户名不能为空", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 16, message: "密码长度6-16之间", trigger: "blur" }
+        ]
+      }
     };
   },
   methods: {
     //编辑点击事件
-    handleEdit(index, row) {
-      console.log(index);
-      console.log(row);
+    async handleEdit(index, row) {
+      // console.log(index);
+      // console.log(row);
+      let res = await this.$axios.get(`users/${row.id}`);
+      // console.log(res);
+      this.editForm = res.data.data;
+      if (res.data.meta.status === 200) {
+        //弹框
+        this.editFormFormVisible = true;
+      }
     },
     //搜索用户
     async search() {
@@ -90,14 +179,69 @@ export default {
       this.userList = res.data.data.users;
     },
     //状态改变
-    stateChange(row){
+    stateChange(row) {
       //调用接口
-      this.$axios.put(`users/${row.id}/state/${row.me_state}`)
+      this.$axios.put(`users/${row.id}/state/${row.me_state}`);
+    },
+    //新增提交 提交事件
+    submitAdd(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          // alert("submit!");
+          //调用接口
+          let res = await this.$axios.post("users", this.addForm);
+          //如果新增成功,重新获取用户列表
+          // console.log(res);
+          if (res.data.meta.status === 201) {
+            this.search();
+          }
+          //关闭弹框
+          this.addFormVisible = false;
+        } else {
+          this.$message.error("请重新输入");
+          return false;
+        }
+      });
+    },
+    //保存修改
+    async submitAdd(formName) {
+      //发请求
+      let res = await this.$axios.put(`users/${this.editForm.id}`, {
+        email: this.editForm.email,
+        mobile: this.editForm.mobile
+      });
+      // console.log(res);
+      if (res.data.meta.status === 200) {
+        this.search();
+      }
+      //关闭弹框
+      this.editFormFormVisible = false;
+    },
+    //删除用户
+    delOne(row) {
+      this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async() => {
+          //axios
+          let res=await this.$axios.delete(`users/${row.id}`);
+          if(res.data.meta.status===200){
+            this.search();
+          }
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     }
   },
   //接口调用
   created() {
-    this.search()
+    this.search();
   }
 };
 </script>
