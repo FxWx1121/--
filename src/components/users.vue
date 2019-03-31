badcc9742b1a3550c970e66c1b188a25e5891e4e
FxWx1121 <572931374@qq.com>
2019-03-31 15:23:38 +0800

    表格列表的渲染完成

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="users-container">
     <!-- 头部面包屑 -->
-    <el-breadcrumb class='my-bread' separator-class="el-icon-arrow-right">
+    <el-breadcrumb class="my-bread" separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-    </el-breadcrumb><el-row>
+    </el-breadcrumb>
+    <el-row>
       <el-col :span="6">
-        <el-input placeholder="请输入内容" class="input-with-select">
+        <el-input placeholder="请输入内容" v-model="sendData.query" class="input-with-select">
           <el-button slot="append" icon="el-icon-search"></el-button>
         </el-input>
       </el-col>
@@ -16,61 +17,91 @@
       </el-col>
     </el-row>
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table :data="userList" style="width: 100%" border>
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="mobile" label="电话"></el-table-column>
+      <el-table-column prop="mg_state" label="用户状态">
+        <!-- scope 是一个名字 -->
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <!-- scope 是一个名字 -->
+        <template slot-scope="scope">
+          <!-- 我们可以通过scope.$index 获取索引 scope.row获取这一行的数据 -->
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-edit"
+            @click="handleEdit(scope.$index, scope.row)"
+            plain
+          ></el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" plain></el-button>
+          <el-button type="warning" size="mini" icon="el-icon-check" plain></el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 分页 -->
     <el-pagination
-      :page-sizes="[100, 200, 300, 400]"
-      :page-size="100"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="sendData.pagesize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
+      :total="total"
     ></el-pagination>
   </div>
 </template>
 
 <script>
 export default {
+  name: "users",
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      //总条数
+      total: 0,
+      //接口数据
+      sendData: {
+        query: "",
+        pagenum: 1,
+        pagesize: 10
+      },
+      //用户数组
+      userList: []
     };
+  },
+  methods: {
+    //编辑点击事件
+    handleEdit(index, row) {
+      console.log(index);
+      console.log(row);
+    }
+  },
+  //接口调用
+  async created() {
+    let res = await this.$axios.get("users", {
+      headers: {
+        Authorization: window.sessionStorage.getItem("token")
+      },
+      params: this.sendData
+    });
+    // console.log(res);
+    this.total=res.data.data.total;
+    this.userList=res.data.data.users;
   }
 };
 </script>
 
 <style lang="scss">
-.users-container{
-    background-color: #e9eef3;
-    height: 100%;
-    .my-bread{
-        background-color: #d3dce6;
-        // height: 50px;
-        line-height: 50px;
-        padding-left: 10px;
-    }
+.users-container {
+  background-color: #e9eef3;
+  height: 100%;
+  .my-bread {
+    background-color: #d3dce6;
+    // height: 50px;
+    line-height: 50px;
+    padding-left: 10px;
+  }
 }
 </style>
